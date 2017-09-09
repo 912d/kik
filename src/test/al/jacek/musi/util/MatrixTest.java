c0fa3584936bd01fdaf62cb4934c8771742bfdc5
Jack Musial <jacek@musi.al>
2017-09-09 22:56:52 +0200

    Extended MatrixTest

--- src/test/al/jacek/musi/util/MatrixTest.java
+++ src/test/al/jacek/musi/util/MatrixTest.java
@@ -4,30 +4,34 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.*;
 
 class MatrixTest {
-    Matrix matrix = new Matrix();
+    Matrix matrix;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        matrix.putOneToMatrix(1);
-        matrix.putOneToMatrix(2);
-        matrix.putOneToMatrix(3);
+        matrix = new Matrix();
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
-        assert((matrix.didUserWon() == 1));
+        //assert((matrix.didUserWon() == 1));
     }
 
     @org.junit.jupiter.api.Test
     void putOneToMatrix() {
+        matrix.putOneToMatrix(-11);
+        matrix.putOneToMatrix(1);
+        matrix.putOneToMatrix(2);
+        matrix.putOneToMatrix(4);
     }
 
     @org.junit.jupiter.api.Test
     void putTwoToMatrix() {
+        throw new java.lang.UnsupportedOperationException();
     }
 
     @org.junit.jupiter.api.Test
     void didUserWon() {
+        System.out.print(matrix.didUserWon());
     }
 
 }
